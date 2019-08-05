0978799bef75ae7f0bf1605cbc7232e6aa490e62
kazuki <pharcydetip@gmail.com>
2019-08-06 01:00:37 +0900

    新しく上場した銘柄を取得し、DBにinsertする処理に進捗表示用のtqdmを追加

--- chapter2/get_new_brands.py
+++ chapter2/get_new_brands.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
-from pyquery import PyQuery
 import datetime
 import sqlite3
+
+from pyquery import PyQuery
+from tqdm import tqdm
+
 
 def new_brands_generator():
     url = 'http://www.jpx.co.jp/listing/stocks/new/index.html'
     q = PyQuery(url)
-    for d, i in zip(q.find('tbody > tr:even > td:eq(0)'),
-                    q.find('tbody > tr:even span')):
+    for d, i in tqdm(zip(q.find('tbody > tr:even > td:eq(0)'),
+                    q.find('tbody > tr:even span')), desc="新上場銘柄をパースし、DBにinsert..."):
         date = datetime.datetime.strptime(d.text, '%Y/%m/%d').date()
         yield (i.get('id'), date)
 
